c77796cea29fa35057c8d559a1a599722abaf1a9
CecileG <cecile.gal.14@ucl.ac.uk>
2026-09-22 20:24:03 +0200

    Fixed untracked files

--- curE_ET_99_plotGaze.m
+++
@@ -1,5 +0,0 @@
-%% Plot gaze overlayed on image per baby, trial and period
-visitNo = 1;
-addpath("src");
-load(sprintf("data/eyetracking/Visit%d/FORJULIETA_all-visit%d-clean-small-fix.mat",visitNo,visitNo));
-fET_PlotGazeTrials(T);
